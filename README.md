4a8305edb63db4e6eb2df8bca51e6c0d82468e7d
MainaTony <tonymaina36@gmail.com>
2019-08-01 11:39:02 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-7bw7na
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-7bw7na)
---
+++ src/app/products.ts
@@ -0,0 +1,24 @@
+export const products = [
+  {
+    name: 'Kamulu',
+    price: 2000000,
+    description: 'Next to SGR'
+  },
+  {
+    name: 'Naivasha Lakeview',
+    price: 1500000,
+    description: '2 kilometres from the lake'
+  },
+  {
+    name: 'Tatu City',
+    price: 10000000,
+    description: 'Next to superhighway'
+  }
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
